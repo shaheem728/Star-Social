4f442b135a5f5df0f8506bdbaef001cd5371f8fb
shaheem <shaheemmahmood728@gmail.com>
2025-03-02 00:21:55 +0530

    Added login and signup functionality with Bootstrap styling and updated URLs and templates accordingly.

--- simplesocial/accounts/urls.py
+++ simplesocial/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+app_name = 'accounts'
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('signup/',views.SignUp.as_view(),name='signup')
 ]
--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('',views.HomePage.as_view(),name='home'),
+    path('accounts/', include('accounts.urls',namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

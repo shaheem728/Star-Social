e90007a84eb42f6a5b1abdef3945f3ed297c8c7b
shaheem <shaheemmahmood728@gmail.com>
2025-03-03 09:44:14 +0530

    `Added join and leave functionality for groups`

--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.ListGroups.as_view(),name='all'),
     path('new/',views.CreateGroup.as_view(),name='create'),
     path('post/in/<str:slug>/',views.SingleGroup.as_view(),name='single'),
+    path('join/<str:slug>/',views.JoinGroup.as_view(),name='join'),
+    path('leave/<str:slug>/',views.LeaveGroup.as_view(),name='leave'),
 ]
--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
 # Create your views here.
 #Group Views.py
 from django.urls import reverse
-from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView,RedirectView
 from groups.models import Group,GroupMember
 
 class CreateGroup(LoginRequiredMixin,CreateView):
@@ -15,3 +16,32 @@ class SingleGroup(DetailView):
 
 class ListGroups(ListView):
     model = Group
+class JoinGroup(LoginRequiredMixin,RedirectView) :
+    def get_redirect_url(self):
+        return reverse("groups:single",kwargs={'slug':self.kwargs.get('slug')})
+    def get(self,request,*args, **kwargs):
+        group = get_object_or_404(Group,slug=self.kwargs.get('slug'))
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+        except IntegrityError:
+            messages.warning(self.request,"You are already a member of this group")
+        else:
+            messages.success(self.request,"You are now a member of this group")
+
+        return super().get(request,*args, **kwargs)    
+
+class LeaveGroup(LoginRequiredMixin,RedirectView):
+    def get_redirect_url(self):
+        return reverse("groups:single",kwargs={'slug':self.kwargs.get('slug')})
+    def get(self,reguest,*args, **kwargs):
+        try:
+            membership = GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except GroupMember.DoesNotExist:
+            messages.warning(self.request,"You are not a member of this group")
+        else:
+            membership.delete()
+            messages.success(self.request,"You have successfully left the group")
+        return super().get(request,*args, **kwargs)    
